4c4e422d14eeae90356a2017b5d0135c5ea087a1
zWellingtonLima <0wellingtonlima1@gmail.com>
2025-03-11 19:12:50 +0000

    refac: change react-router components usage from main to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
+import { Dashboard } from "./pages/app/dashboard";
+import { SignIn } from "./pages/auth/sign-in";
+
 export function App() {
-  return <h1 className="flex flex-col bg-red-500 text-2xl">Pizza Dashboard</h1>;
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Dashboard />} />
+          </Route>
+
+          <Route path="/sign-in" element={<AuthLayout />}>
+            <Route index element={<SignIn />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,11 @@ import "./index.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
 
 import { App } from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
