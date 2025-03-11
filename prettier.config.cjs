dd4ad2aa62c992c903b2d04c6fa2e62edc96b07b
zWellingtonLima <0wellingtonlima1@gmail.com>
2025-03-11 19:10:55 +0000

    chore: remove non-existent plugin and rename simple-import-sort

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,14 +3,14 @@ const config = {
     sourceType: "module",
     ecmaVersion: "latest",
   },
-  plugins: ["simple-import-sort", "import", "prettier-plugin-tailwindcss"],
+  plugins: ["eslint-plugin-simple-import-sort", "prettier-plugin-tailwindcss"],
   rules: {
-    "simple-import-sort/imports": "error",
+    "eslint-plugin-simple-import-sort/imports": "error",
     "import/first": "error",
     "import/newline-after-import": "error",
     "import/no-duplicates": "error",
   },
-}
+};
 
 // eslint-disable-next-line no-undef
 module.exports = config;
